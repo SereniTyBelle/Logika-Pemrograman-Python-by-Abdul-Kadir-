7d5f4eb9db1c29c53a85f1ec1e234ec0debda754
Jeslyne <64067058+SereniTyBelle@users.noreply.github.com>
2021-10-17 22:34:52 +0700

    Update bab 4.py

--- bab 4.py
+++ bab 4.py
@@ -1,17 +1,17 @@
 #Bab 4
 
 #huruf.py
-#kar = input("Masukkan karakter = ")
-#huruf = (kar >= "A" and kar <= "Z") or \
-#    (kar >= "a" and kar <= 'z')
-#hasil = 'huruf' if huruf \
-#    else 'bukan huruf'
-#print (kar, 'merupakan', hasil)
+kar = input("Masukkan karakter = ")
+huruf = (kar >= "A" and kar <= "Z") or \
+    (kar >= "a" and kar <= 'z')
+hasil = 'huruf' if huruf \
+    else 'bukan huruf'
+print (kar, 'merupakan', hasil)
 
 #nomor 10
-kar = input ("Masukkan karakter = ")
-vokal = (kar == ('A' or "I" or 'U' or 'E' or 'O')) or \
-    (kar == ('a' or 'i' or 'u' or 'e' or 'o'))
+kar1 = input ("Masukkan karakter = ")
+vokal = (kar1 == ('A' or "I" or 'U' or 'E' or 'O')) or \
+    (kar1 == ('a' or 'i' or 'u' or 'e' or 'o'))
 hasil = "merupakan huruf vokal" if vokal \
     else 'bukan huruf vokal'
-print (kar, hasil)
+print (kar1, hasil)
