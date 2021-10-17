2da8f797d13479758655b78dd034533010834765
Jeslyne <64067058+SereniTyBelle@users.noreply.github.com>
2021-10-17 22:34:09 +0700

    Update bab 3.py

--- bab 3.py
+++ bab 3.py
@@ -1,18 +1,18 @@
 #Nomor 17
-#diskon=0.1
-#total_pemb=float(input("Masukkan total pembelian: "))
-#print("Besar diskon: 10%")
-#total_disk= total_pemb * diskon
-#total_penj= total_pemb - total_disk
-#print('Total yang dibayarkan: %.2f' %(total_penj))
+diskon=0.1
+total_pemb=float(input("Masukkan total pembelian: "))
+print("Besar diskon: 10%")
+total_disk= total_pemb * diskon
+total_penj= total_pemb - total_disk
+print('Total yang dibayarkan: %.2f' %(total_penj))
 
 #Nomor 18
-#badu=int(input('Masukkan umur Badu: '))
-#x=badu
-#x-=2
-#print('Umur Badu adalah ', badu)
-#print('Umur Sita adalah ', x)
-#print('Jumlah umur mereka adalah ', badu + x)
+badu=int(input('Masukkan umur Badu: '))
+x=badu
+x-=2
+print('Umur Badu adalah ', badu)
+print('Umur Sita adalah ', x)
+print('Jumlah umur mereka adalah ', badu + x)
 
 #Nomor 15
 a=int(input("Masukkan angka: "))
